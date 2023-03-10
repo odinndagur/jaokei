0cb8e83fd5ceeb0b8ec214a7b2520cd5e2b678f5
Óðinn Dagur Bjarnason <odinnd@gmail.com>
2023-03-10 11:03:56 +0000

    add docs folder for pages

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
+import { useLoader } from '@react-three/fiber';
+import { Model } from './Ingolfsstraeti2';
+
+function Box(props) {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef()
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false)
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (ref.current.rotation.x += delta))
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : props.scale || 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
+
+function AegirHead (props){
+  const obj = useLoader(OBJLoader, '/head3d.obj')
+  const ref = useRef()
+  const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false)
+
+  useFrame((state, delta) => (ref.current.rotation.y += delta))
+  return (
+    <mesh>
+      <primitive
+        {...props}
+        object={obj}
+        scale={0.01}
+        ref={ref}
+        onClick={(event) => click(!clicked)}
+        onPointerOver={(event) => hover(true)}
+        onPointerOut={(event) => hover(false)}
+      />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
+
+export default function App() {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+      <pointLight position={[-10, -10, -10]} />
+      <Model/>
+      <Box position={[-1.2, 0, 0]} scale={0.2} />
+      <Box position={[1.2, 0, 0]} />
+      <AegirHead position={[9,3,0]} />
+      <OrbitControls />
+    </Canvas>
+  )
+}
